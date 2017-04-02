a8c1f78356d4bc4f0770f89111aead653bd990ca
ScarletMcLearn <ratul_shams@yahoo.com>
2017-04-02 09:47:13 -0400

    Add files via upload

---
+++ IUB-DataStructures-master/CSC203_Sec_03_Assignment_04_1621176.cpp
@@ -0,0 +1,300 @@
+#include <iostream>
+#include "DisplayTreeHeader.cpp"
+using namespace std;
+class BSTree{
+    /*
+struct TreeNode
+	{
+		float data;
+		struct TreeNode *left;
+		struct TreeNode *right;
+	};
+	*/
+
+
+public:
+    TreeNode *r;
+    BSTree()
+    {
+        r=NULL;
+    }
+    /*void recInsert(TreeNode *root, TreeNode *newNode)
+    {
+        if(r==NULL)
+        {
+            r=newNode;
+        }
+        else if(newNode->data<root->data)
+        {
+            if(root->left==NULL)
+                root->left=newNode;
+            else recInsert(root->left,newNode);
+        }
+        else
+        {
+            if(root->right==NULL)
+                root->right=newNode;
+            else recInsert(root->right,newNode);
+        }
+    }*/
+    TreeNode* recInsert(TreeNode *root, TreeNode *newNode)
+    {
+        if(root==NULL)
+        {
+            return newNode;
+        }
+        else if(newNode->data<root->data)
+        {
+            root->left=recInsert(root->left,newNode);
+        }
+        else
+        {
+            root->right=recInsert(root->right,newNode);
+        }
+
+        return root;
+    }
+    float findMinBST(TreeNode *root)
+    {
+        if(root->left==NULL)
+            return root->data;
+        return findMinBST(root->left);
+    }
+    float findMaxBST(TreeNode *root)
+    {
+        if(root->right==NULL)
+            return root->data;
+        return findMaxBST(root->right);
+    }
+    TreeNode* findMaximum(TreeNode *root)
+    {
+        if(root->right==NULL)
+            return root;
+        return findMaximum(root->right);
+    }
+
+    bool findBST(TreeNode *root, float sVal)
+    {
+        if(root==NULL)
+        {
+            return false;
+        }
+        else if(sVal<root->data)
+        {
+            return findBST(root->left,sVal);
+        }
+        else if(sVal>root->data)
+        {
+            return findBST(root->right,sVal);
+        }
+        else{
+            return true;
+        }
+    }
+    bool recDelete(TreeNode *root, float sVal, TreeNode *parent=NULL, int child=0)
+    {
+        TreeNode *delNode, *largest;
+
+        if(root==NULL) return false;
+        if(sVal<root->data)
+        {
+            return recDelete(root->left,sVal,root,0);
+        }
+        else if(sVal>root->data)
+        {
+            return recDelete(root->right,sVal,root,1);
+        }
+        else{
+            if(root->left==NULL)
+            { //node have only right child
+                if(parent==NULL) r=root->right;
+                else if(child==0)
+                {
+                    parent->left=root->right;
+                    delete root;
+                    return true;
+                }
+                else{
+                    parent->right=root->right;
+                    delete root;
+                    return true;
+                }
+            }
+            else if(root->right==NULL)
+            {
+                //node have only left child
+                if(parent==NULL) r=root->left;
+                else if(child==0)
+                {
+                    parent->left=root->left;
+                    delete root;
+                    return true;
+                }
+                else{
+                    parent->right=root->left;
+                    delete root;
+                    return true;
+                }
+            }
+            else{
+               //node have both children
+               delNode=root;
+               largest=findMaximum(root->left);
+               delNode->data=largest->data;
+               return recDelete(delNode->left,largest->data,delNode,0);
+
+            }
+
+        }
+
+    }
+    void bstDelete()
+    {
+        float sVal;
+        cout<<"\nEnter Data to Delete: ";
+        cin>>sVal;
+        if(recDelete(r,sVal)==true)
+        {
+            cout<<"Successfully Deleted!!"<<endl;
+        }
+        else{
+            cout<<"Not Deleted or Data not found!!"<<endl;
+        }
+    }
+
+
+    void bstinsert()
+    {
+        TreeNode *newNode;
+        newNode=new TreeNode;
+        cout<<"\nEnter Data for Node: ";
+        cin>>newNode->data;
+        newNode->left=NULL;
+        newNode->right=NULL;
+        //recInsert(r,newNode);
+        r=recInsert(r,newNode);
+    }
+    void preorder(TreeNode *root)
+    {
+        if(root==NULL) return;
+        cout<<root->data<<" ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+    void inorder(TreeNode *root)
+    {
+        if(root==NULL) return;
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+    void postorder(TreeNode *root)
+    {
+        if(root==NULL) return;
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->data<<" ";
+    }
+
+
+
+
+
+    void TreeTracer(TreeNode *root, int level, int lvl_storer)
+    {
+        TreeNode * current = root;
+
+        if (current == NULL)
+        {
+            return;
+        }
+
+
+        TreeTracer(current -> left, level+1);
+
+
+        if (current)
+        {
+            cout<<"0";
+        }
+
+        if (level == lvl_storer)
+        {
+            cout<<", "
+        }
+
+
+
+
+        TreeTracer(current -> right, level+1);
+
+        if (current)
+        {
+            cout<<"1";
+        }
+
+        // TreeTracer(current -> left);
+
+        if (level == lvl_storer)
+        {
+            cout<<", "
+        }
+
+    }
+
+
+
+    void displayTracer(TreeNode *root)
+    {
+        cout<<"Display code: ";
+
+        TreeTracer(root);
+    }
+};
+
+int main()
+{
+    Tree dObj;
+    BSTree obj;
+    char c;
+    float sVal;
+
+    do
+    {
+    obj.bstinsert();
+    cout<<"\nEnter another? Y/N: ";
+    cin>>c;
+    }while(c=='y'||c=='Y');
+
+    dObj.prnttree(obj.r, dObj.findHeight(obj.r));
+/*
+    cout<<"\nMaximum value: "<<obj.findMaxBST(obj.r)<<endl;
+    cout<<"\nMinimum value: "<<obj.findMinBST(obj.r)<<endl;
+    cout<<"Enter a search value:"<<endl;
+    cin>>sVal;
+    if(obj.findBST(obj.r,sVal)==true)
+    {
+        cout<<"The given value is found in the BST"<<endl;
+    }
+    else{
+        cout<<"Data not found!"<<endl;
+    }
+
+
+    cout<<endl<<"Preorder display: "<<endl;;
+    obj.preorder(obj.r);
+    cout<<endl;
+*/
+    do
+    {
+    obj.bstDelete();
+    dObj.prnttree(obj.r, dObj.findHeight(obj.r));
+    cout<<"\nEnter another? Y/N: ";
+    cin>>c;
+    }while(c=='y'||c=='Y');
+
+
+
+
+
+}
---
+++ IUB-DataStructures-master/HeapQueue.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<iomanip>
+using namespace std;
+class Queue {
+
+    struct Node{
+      float pri;
+      int key;
+   };
+   //int * Heap;
+    Node *Heap;
+	int counter;	// number of elements
+	int maxSize;	// size of array queue
+	double* values;	// element array
+
+public:
+	Queue(int size = 10);			// constructor
+	~Queue() { delete [] Heap; }	// destructor
+	bool IsEmpty(void);
+	bool IsFull(void);
+	bool Enqueue(double x,float pri);
+	bool Dequeue(double & x,float pri);
+	void DisplayQueue(void);
+	void rebuildHeap(Node  Heap[], int  parent,  int  n);
+
+};
+Queue::Queue(int size /* = 10 */) {
+	Heap		=	new Node[size];
+	maxSize		=	size;
+	counter		=	0;
+}
+bool Queue::IsEmpty() {
+	if (counter)	return false;
+	else			return true;
+}
+bool Queue::IsFull() {
+	if (counter < maxSize)	return false;
+	else				return true;
+}
+bool Queue::Enqueue(double x, float pri) {
+	int i;
+	if (IsFull()) {
+		cout << "Error: the queue is full." << endl;
+		return false;
+	}
+	else {
+		i = counter;
+        while ((i!=0)&&(pri>Heap[(i-1)/2].pri)) {
+        Heap[i] = Heap[(i-1)/2];
+        i = (i-1)/2;
+        }
+        Heap[i].key= x;
+        Heap[i].pri= pri;
+		// update counter
+		counter++;
+		return true;
+	}
+}
+void Queue::rebuildHeap(Node  Heap[], int  parent,  int  n)
+       {
+           int  child  =  2 * parent + 1;	// set child to root’s left child, if any
+           if( child  <  n )		// if root’s left child exists . . .
+           {
+               int  rightChild  =  child + 1;
+               if( rightChild  <  n  &&  Heap[rightChild].pri>Heap[child].pri)
+               child = rightChild;	// child indicates the larger item
+               if(Heap[parent].pri<Heap[child].pri)
+               {
+                   swap( Heap[parent],  Heap[child]);
+                   rebuildHeap( Heap,child,n);
+               }
+          }
+       }
+bool Queue::Dequeue(double & x, float pri) {
+	if (IsEmpty()) {
+		cout << "Error: the queue is empty." << endl;
+		return false;
+	}
+	else {
+		// retrieve the front item
+		x=Heap[0].key;
+		pri=Heap[0].pri;
+		Heap[0]=Heap[counter-1];
+		// update counter
+		counter--;
+		rebuildHeap(Heap,0,counter);
+		return true;
+	}
+}
+void Queue::DisplayQueue() {
+	Node *tmp;
+	int tCounter=counter;
+	tmp=new Node[counter];
+	for(int i=0;i<counter;i++)
+        tmp[i]=Heap[i];
+
+    cout<<"\nQueue elements: "<<endl;
+    for( ;tCounter>0; )
+    {
+        cout<<tmp[0].key<<"("<<tmp[0].pri<<")"<<" ";
+        tmp[0]=tmp[tCounter-1];
+        tCounter=tCounter-1;
+        rebuildHeap(tmp,0,tCounter);
+    }
+}
+int main(void) {
+	Queue queue(5);
+
+	queue.Enqueue(100,50);
+	queue.Enqueue(12,2);
+	queue.Enqueue(15,3);
+	queue.Enqueue(200,50);
+	queue.Enqueue(1,100);
+
+	queue.DisplayQueue();
+	return 0;
+}
+
+

641edada7211e1096e521f4c1215f0cd5185ad9d
ScarletMcLearn <ratul_shams@yahoo.com>
2017-05-14 15:22:14 -0400

    Add files via upload

---
+++ CSE 211 - Algorithms/Insertion Sort.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+
+void insertion_sort(int arr[], int sz);
+
+int * dm_array(int sz);
+
+void display(int * arr, int sz);
+
+
+
+int main()
+{
+    int arr[] = {4, 2, 66, 1, 4};
+
+    insertion_sort(arr, 5);
+
+    display(arr, 5);
+
+    cout<<endl;
+
+    int * arr_2 = dm_array(5);
+
+    display(arr_2, 5);
+
+
+
+    return 0;
+}
+
+
+void insertion_sort(int arr[], int sz)
+{
+    int i, j, v;
+
+    for (i = 1; i < sz; i++)
+    {
+        v = arr[i];
+        j = i;
+
+        while (j > 0 && arr[j-1] > v)
+        {
+            arr[j] = arr[j-1];
+            j--;
+        }
+
+        arr[j] = v;
+    }
+}
+
+
+int * dm_array(int sz)
+{
+    int * arr = new int[sz];
+
+    cout<<"Please enter " << sz << " value(s) seperated by spaces: ";
+
+    for (int iter = 0; iter < sz; iter++)
+    {
+        cin>> arr[iter];
+    }
+
+    return arr;
+}
+
+
+
+void display(int * arr, int  sz)
+{
+    for (int iter =0; iter < sz; iter++)
+        cout<< arr[iter] << " ";
+}
